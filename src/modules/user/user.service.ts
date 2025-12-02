33e13e1aa4aab29c7fda4fad8e1a93cd42dc8109
Ayat Ayoub <ayatayoub492@gmail.com>
2025-12-02 22:48:09 +0200

    user servive update

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -9,7 +9,6 @@ import { PaginatedResult, PaginationQueryType } from 'src/types/util.types';
 
 @Injectable()
 export class UserService {
-  prismaService: any;
   constructor( private readonly prismaServise : DatabaseService) {}
 
    create(registerDTO: RegisterDTO) {
@@ -30,12 +29,12 @@ export class UserService {
 findAll(
     query: PaginationQueryType,
   ): Promise<PaginatedResult<Omit<User, 'password'>>> {
-    return this.prismaService.$transaction(async (prisma) => {
+    return this.prismaServise.$transaction(async (prisma) => {
     // Step 1: Extract pagination values from query
     // page -> current page number
     // skip -> how many items to skip
     // take -> how many items to return
-      const pagination = this.prismaService.handleQueryPagination(query);
+      const pagination = this.prismaServise.handleQueryPagination(query);
     // Step 2: Get all users with pagination
     // - "skip" and "take" are added using spread operator
     // - "page" is removed because Prisma does not accept it
@@ -50,7 +49,7 @@ findAll(
       // Step 3: Get the total number of users
       const count = await prisma.user.count();
       // Step 4: Build pagination metadata
-      const meta = this.prismaService.formatPaginationResponse({
+      const meta = this.prismaServise.formatPaginationResponse({
         page: pagination.page,
         count,
         limit: pagination.take,
